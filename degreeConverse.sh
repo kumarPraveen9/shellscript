ebd70388b96e64b7feed747a4ded0ba62b8d9963
kumarPraveen9 <120436370+kumarPraveen9@users.noreply.github.com>
2022-12-16 22:02:55 +0530

    find degF or degC based on their Conversion Selection. Use Case Statement and ensure that the inputs are within the Freezing Point

--- degreeConverse.sh
+++ degreeConverse.sh
@@ -1,4 +1,4 @@
-#Help user find degF or degC based on their Conversion Selection. Use
+#Help user find degF or degC based on their Conversion Selection. Use the
 #Case Statement and ensure that the inputs are within the Freezing Point (0 °C / 32 °F ) and the Boiling Point of Water ( 100 °C / 212 °F )
 #a. degF = (degC * 9/5) + 32
 #b. degC = (degF – 32) * 5/9
