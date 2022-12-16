654c76dab4c2e4f24fcfb34d93547f6a250e33ea
kumarPraveen9 <120436370+kumarPraveen9@users.noreply.github.com>
2022-12-16 22:12:05 +0530

    program that takes a command-line argument n and prints a table of the  powers of 2 that are less than or equal to 2^n.

--- power2for.sh
+++ power2for.sh
@@ -1,4 +1,4 @@
-#a program that takes a command-line argument n and prints a table of the powers of 2 that are less than or equal to 2^n.
+# program that takes a command-line argument n and prints a table of the powers of 2 that are less than or equal to 2^n.
 
 read -p "enter a number : " n
 echo ""
