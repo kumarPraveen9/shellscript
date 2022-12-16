8bd33b0fab3fb84ffb292858881083559a33d5db
kumarPraveen9 <120436370+kumarPraveen9@users.noreply.github.com>
2022-12-16 22:11:09 +0530

    program that takes a command-line argument n and prints a table of the powers of 2 that are less than or equal to 2^n till 256 is reached..

--- power2Repitation.sh
+++ power2Repitation.sh
@@ -1,3 +1,5 @@
+#program that takes a command-line argument n and prints a table of the powers of 2 that are less than or equal to 2^n till 256 is reached..
+
 read -p "enter a number : " n
 m=1
 for ((i=0;i<=n;i++))
