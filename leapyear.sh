a7ae8ac82370c9e9dae7da23843b07dda7fc32d1
kumarPraveen9 <120436370+kumarPraveen9@users.noreply.github.com>
2022-12-16 22:25:40 +0530

    a program that takes a year as input and outputs the Year is a Leap Year or not a Leap Year. A Leap Year checks for 4 Digit Number, Divisible by 4 and not 100 unless divisible by 400.

--- leapyear.sh
+++ leapyear.sh
@@ -1,3 +1,7 @@
+#a program that takes a year as input and outputs the Year is a Leap Year or not
+#a Leap Year. A Leap Year checks for 4 Digit Number, Divisible by 4 and not 100 unless
+#divisible by 400.
+
 read -p "Give input as year: " year
 
 if [ $year -gt 1000 ]&&[ $year -le 9999 ]&&[ $(($year%400)) -eq 0 ]
