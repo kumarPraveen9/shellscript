32a7df88b792445cd7f2672e382f331c58dbe80c
kumarPraveen9 <120436370+kumarPraveen9@users.noreply.github.com>
2022-12-16 22:06:18 +0530

    gambler starts with Rs 100 and places Re 1 bet until he/she goes broke i.e. no more money to gamble or reaches the goal of Rs 200. Keeps track of number of times won and number of bets made.

--- gamble.sh
+++ gamble.sh
@@ -1,3 +1,6 @@
+#gambler starts with Rs 100 and places Re 1 bet until he/she goes broke i.e. no more money to gamble or reaches the
+#goal of Rs 200. Keeps track of number of times won and number of bets made.
+
 money=1000
 wins=0
 bets=0
